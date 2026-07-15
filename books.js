977a56df25e9588d8282b974cb68954ee5fcbd41
Ryan <Woodedcode@gmail.com>
2026-07-15 10:45:09 -0500

    Enhance book sorting and price display logic
    Updated sorting logic to prioritize salePrice if available. Modified price display to use priceHTML function for better formatting.

--- books.js
+++ books.js
@@ -4,9 +4,9 @@ function renderBooks(filter) {
   let books = getBooks();
 
   if (filter === 'LOW_TO_HIGH') {
-    books.sort((a,b) => a.originalPrice - b.originalPrice);
+    books.sort((a,b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice));
   } else if (filter === 'HIGH_TO_LOW') {
-    books.sort((a,b) => b.originalPrice - a.originalPrice);
+    books.sort((a,b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice));
   } else if (filter === 'RATING') {
     books.sort((a,b) => b.rating - a.rating);
   }
@@ -24,7 +24,7 @@ function renderBooks(filter) {
        ${ratingsHTML(book.rating)}
      </div>
      <div class="book__price">
-       <span">$${book.originalPrice.toFixed(2)}</span>
+       ${priceHTML(book.originalPrice, book.salePrice)}
      </div>
    </div>`
     })
@@ -44,6 +44,12 @@ function ratingsHTML(rating) {
   return ratingHTML;
 }
 
+function priceHTML(originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`
+  } 
+  return `<span class="book__price--normal">$${originalPrice}</span> ${salePrice.toFixed(2)}`
+}
 
 function filterBooks(event) {
     renderBooks(event.target.value)
@@ -146,4 +152,3 @@ function getBooks() {
     },
   ];
 }
-
